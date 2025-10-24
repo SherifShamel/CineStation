ee9fc0f9cc697a13e932ea611cc5e86e13a46193
sherif <sherif_shamel15@yahoo.com>
2025-10-24 20:43:03 +0300

    fixed the app.routes.server for the clientMode.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {path:'movieslists/:genreTitle',renderMode:RenderMode.Client},
+  {path:'serieslists/:genreTitle',renderMode:RenderMode.Client},
+  {path:'moviedetails/:m_id',renderMode:RenderMode.Client},
+  {path:'seriesdetails/:m_id',renderMode:RenderMode.Client},
   {
     path: '**',
     renderMode: RenderMode.Prerender
